4d9afdfeea6c9b7c3b9c2809a8c9b7d6755b2a29
HiroZero <57805329+HiroZero@users.noreply.github.com>
2019-12-11 07:55:45 -0700

    Create globals.h

---
+++ globals.h
@@ -0,0 +1,18 @@
+#pragma once
+//constants: variables that shouldnt change once the game starts
+const float FPS = 60;
+const int SCREEN_W = 800;
+const int SCREEN_H = 800;
+const int BARREL_SIZE = 20;
+const int PLATFORM_WIDTH = 80;
+const int PLATFORM_HEIGHT = 30;
+
+
+//enumerators
+
+enum MYKEYS { KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT };
+
+enum DIRECTIONS { UP, DOWN, LEFT,RIGHT};
+
+enum ITEM_IDS {BARREL, PLATFORM, PLAYER};
+
